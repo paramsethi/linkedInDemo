75c9c86d34abef7327d70815387c530e3f7ff814
param sethi <param.sethi@gmail.com>
2011-09-26 23:08:26 -0400

    removed unused function and updated message

--- src/main/webapp/js/lndemo.js
+++ src/main/webapp/js/lndemo.js
@@ -1,14 +1,8 @@
 function validateForm() {
 	var x = document.forms["search"]["query"].value;
 	if (x == null || x == "" || x=="Search...") {
-		alert("Search query must be filled out and should contain valid alpha numeric characters.");
+		alert("Search query must be filled out.");
 		return false;
 	}
 }
 
-function checkSearchQuery(qry){
-	if (qry != null && qry != "") {
-		if(qry.indexOf(" ") > -1)
-			document.getElementById("queryMsg").display="";
-	}
-}
