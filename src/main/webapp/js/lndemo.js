edc27c1f9f39abb6eecebd38619913ed56eeec53
param sethi <param.sethi@gmail.com>
2011-09-26 22:04:33 -0400

    Fixed js function

--- src/main/webapp/js/lndemo.js
+++ src/main/webapp/js/lndemo.js
@@ -1,7 +1,14 @@
 function validateForm() {
 	var x = document.forms["search"]["query"].value;
 	if (x == null || x == "" || x=="Search...") {
-		alert("Search query must be filled out");
+		alert("Search query must be filled out and should contain valid alpha numeric characters.");
 		return false;
+	}
+}
+
+function checkSearchQuery(qry){
+	if (qry != null && qry != "") {
+		if(qry.indexOf(" ") > -1)
+			document.getElementById("queryMsg").display="";
 	}
 }
